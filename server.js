3e1931dfe4aac46c2ca501090bfb0632a9198c9e
elderdeveloper15 <jesusangel16300077@gmail.com>
2022-03-21 19:49:08 -0600

    keroku 5

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular.io-example'));
 app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));});
+{res.sendFile(path.join(__dirname+'/dist/angular.io-example/index.html'));});
 
 app.listen(process.env.PORT || 8080);
