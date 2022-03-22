677a165c61c0fab3363dc18ed67836b8a0048034
elderdeveloper15 <jesusangel16300077@gmail.com>
2022-03-21 18:24:57 -0600

    cambios 3

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11,score:11, name: 'Dr Nice' },
-  { id: 12,score:12, name: 'Narco Manta' },
-  { id: 13,score:13, name: 'Bombasto' },
-  { id: 14,score:14, name: 'Celeritas' },
-  { id: 15,score:15, name: 'Magneta' },
-  { id: 16,score:16, name: 'RubberMan' },
-  { id: 17,score:17, name: 'Dynama' },
-  { id: 18,score:18, name: 'Dr IQ' },
-  { id: 19,score:19, name: 'Magma' },
-  { id: 20,score:20, name: 'Tornado' }
+  { id: 11,score:11, name: 'Guybrush Threepwood' },
+  { id: 12,score:12, name: 'Gordon Freeman' },
+  { id: 13,score:13, name: 'Simon Belmont' },
+  { id: 14,score:14, name: 'John Marston' },
+  { id: 15,score:15, name: 'Link' },
+  { id: 16,score:16, name: 'Jill Valentine' },
+  { id: 17,score:17, name: 'Cloud Strife' },
+  { id: 18,score:18, name: 'Kratos' },
+  { id: 19,score:19, name: 'Master Chief' },
+  { id: 20,score:20, name: 'Nathan Drake' }
 ];
