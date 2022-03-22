271d2fc967464043064e1a37806fe3fd2640cf83
elderdeveloper15 <jesusangel16300077@gmail.com>
2022-03-21 19:57:35 -0600

    atom bomb

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Trabajo07-Final'));
+app.use(express.static('./dist/angular.io-example'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
