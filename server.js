d1ba839343f8674042b8946e33029e67798f3843
elderdeveloper15 <jesusangel16300077@gmail.com>
2022-03-21 19:53:46 -0600

    dawdawd

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/angular.io-example'));
-app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/angular.io-example/index.html'));});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/Trabajo07-Final'));
 
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
