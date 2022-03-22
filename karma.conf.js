2fd26b52e5ed58f05b204ef97c9362a1a331eb37
elderdeveloper15 <jesusangel16300077@gmail.com>
2022-03-21 18:05:31 -0600

    Trabajo07

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/trabajo07-final'),
+      dir: require('path').join(__dirname, './coverage/angular.io-example'),
       subdir: '.',
       reporters: [
         { type: 'html' },
