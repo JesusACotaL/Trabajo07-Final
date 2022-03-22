be1f3fc1470b65539a1f6468843ac1e7b1c29b30
elderdeveloper15 <jesusangel16300077@gmail.com>
2022-03-21 18:20:09 -0600

    cambios 2

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,5 @@
 export interface Hero {
   id: number;
+  score: number;
   name: string;
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11,score:11, name: 'Dr Nice' },
+  { id: 12,score:12, name: 'Narco Manta' },
+  { id: 13,score:13, name: 'Bombasto' },
+  { id: 14,score:14, name: 'Celeritas' },
+  { id: 15,score:15, name: 'Magneta' },
+  { id: 16,score:16, name: 'RubberMan' },
+  { id: 17,score:17, name: 'Dynama' },
+  { id: 18,score:18, name: 'Dr IQ' },
+  { id: 19,score:19, name: 'Magma' },
+  { id: 20,score:20, name: 'Tornado' }
 ];
